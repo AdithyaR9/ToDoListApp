acb29e66d418e42c07d4f347ceefd6ea1e5a974c
AdithyaR9 <rocket_saturn@hotmail.com>
2024-03-15 12:08:54 -0500

    checkmark isDone functionality with database

--- ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -6,11 +6,25 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class ProfileViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemTemp = item
+        itemTemp.setDone(!itemTemp.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemTemp.id)
+            .setData(itemTemp.asDictionary())
     }
 }
